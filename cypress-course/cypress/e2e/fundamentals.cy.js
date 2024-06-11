ab6c3e6fc8445ca0846aca69c19a894852b7192c
Jason <jasonetienne1@gmail.com>
2024-06-11 19:48:26 +0100

    adding test data and updating test

--- cypress-course/cypress/e2e/fundamentals.cy.js
+++ cypress-course/cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,6 @@
 describe('Fundamentals test', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/fundamentals')
+    cy.get('[data-test="fundamentals-header"]').contains("Testing Fundamentals")
   })
 })
