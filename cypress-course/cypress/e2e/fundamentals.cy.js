e5d453c32e7b5ffe44414f196e30abc1e6cebc5a
Jason <jasonetienne1@gmail.com>
2024-06-11 19:52:56 +0100

    adding a should contains test

--- cypress-course/cypress/e2e/fundamentals.cy.js
+++ cypress-course/cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,8 @@
 describe('Fundamentals test', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/fundamentals')
-    cy.get('[data-test="fundamentals-header"]').contains("Testing Fundamentals")
+    //cy.get('[data-test="fundamentals-header"]').contains("Testing Fundamentals")
+    //cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)   regex and using i makes it case insensitive
+    cy.get('[data-test="fundamentals-header"]').should("contain.text", "Testing Fundamentals")
   })
 })
