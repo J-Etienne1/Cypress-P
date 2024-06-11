32e5fcabe4cb225eb759300a434775b8243023fc
Jason <jasonetienne1@gmail.com>
2024-06-11 20:21:11 +0100

    adding beforeEach to set up each test and then removing now unnecessary cy.visit for each test

--- cypress-course/cypress/e2e/fundamentals.cy.js
+++ cypress-course/cypress/e2e/fundamentals.cy.js
@@ -1,12 +1,14 @@
 describe("Fundamentals test", () => {
+  beforeEach(() => {
+    cy.visit("/fundamentals")
+  })
+
   it('Contains correct header text', () => {
-    cy.visit('/fundamentals')
     //cy.get('[data-test="fundamentals-header"]').contains("Testing Fundamentals")
     //cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)   regex and using i makes it case insensitive
     cy.get('[data-test="fundamentals-header"]').should("contain.text", "Testing Fundamentals")
   })
   it("Accordion works correctly", () => {
-    cy.visit("/fundamentals")
     cy.contains(/Your tests will exist in a describe block./i).should("not.be.visible")
     //cy.get('[data-test="accordion-item-1"]').click().pause()
     cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
@@ -16,7 +18,6 @@ describe("Fundamentals test", () => {
   })
   // To run only a specific test
   /* it.only("Accordion works correctly ONLY", () => {
-    cy.visit("/fundamentals")
     cy.contains(/Your tests will exist in a describe block./i).should("not.be.visible")
     //cy.get('[data-test="accordion-item-1"]').click().pause()
     cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
