7f4b1464f15135432371b147386f0a56a8e34874
Jason <jasonetienne1@gmail.com>
2024-06-12 20:46:53 +0100

    Update forms.cy.js
    adding comments for various paths and editing to make it more clear

--- cypress-course/cypress/e2e/forms.cy.js
+++ cypress-course/cypress/e2e/forms.cy.js
@@ -3,6 +3,7 @@ describe("form test", () => {
         cy.visit("/forms")
     })
     it("test subscribe from", () => {
+        //Happy Path - correct email format
         cy.contains(/testing forms/i)
         cy.getDataTest("subscribe-form").find("input").as("subscribe-input")
         cy.get("@subscribe-input").type("jasonetienne1@gmail.com")
@@ -12,13 +13,14 @@ describe("form test", () => {
         cy.wait(3000)
         cy.contains(/Successfully subbed: jasonetienne1@gmail.com!/i).should("not.exist")
 
-        //Unhappy Path
+        //Unhappy Path - incorrect email format
         cy.get("@subscribe-input").type("jasonetienne1@gmail.io")
         cy.contains(/invalid email: jasonetienne1@gmail.io!/i).should("not.exist")
         cy.getDataTest("subscribe-button").click()
         cy.contains(/invalid email: jasonetienne1@gmail.io!/i).should("exist")
         cy.wait(3000)
         cy.contains(/invalid email: jasonetienne1@gmail.io!/i).should("not.exist")
+        //Unhappy Path - no input for email
         cy.contains(/fail!/i).should("not.exist")
         cy.getDataTest("subscribe-button").click()
         cy.contains(/fail!/i).should("exist")
