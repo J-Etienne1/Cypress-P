683c375aea35421010d559654c6e615708afd4c7
Jason <jasonetienne1@gmail.com>
2024-06-12 19:59:46 +0100

    using Type Alias for input using .as()

--- cypress-course/cypress/e2e/forms.cy.js
+++ cypress-course/cypress/e2e/forms.cy.js
@@ -4,7 +4,8 @@ describe("form test", () => {
     })
     it("test subscribe from", () => {
         cy.contains(/testing forms/i)
-        cy.getDataTest("subscribe-form").find("input").type("jasonetienne1@gmail.com")
+        cy.getDataTest("subscribe-form").find("input").as("subscribe-input")
+        cy.get("@subscribe-input").type("jasonetienne1@gmail.com")
         cy.contains(/Successfully subbed: jasonetienne1@gmail.com!/i).should("not.exist")
         cy.getDataTest("subscribe-button").click()
         cy.contains(/Successfully subbed: jasonetienne1@gmail.com!/i).should("exist")
