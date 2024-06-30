661d3608e0008966037bf55b3579157cceb7d46d
Jason <jasonetienne1@gmail.com>
2024-06-30 12:51:32 +0100

    testing that removing lis work correctly

--- cypress-course/cypress/e2e/examples.cy.js
+++ cypress-course/cypress/e2e/examples.cy.js
@@ -1,67 +1,76 @@
 describe("Various examples", () => {
-    beforeEach(() => {
-    cy.visit("/examples")
-  })
+  beforeEach(() => {
+    cy.visit("/examples");
+  });
   it("multi-page testing", () => {
     cy.getDataTest("nav-why-cypress").click();
-    cy.location("pathname").should("equal", "/")
+    cy.location("pathname").should("equal", "/");
 
     cy.getDataTest("nav-overview").click();
-    cy.location("pathname").should("equal", "/overview")
+    cy.location("pathname").should("equal", "/overview");
 
     cy.getDataTest("nav-fundamentals").click();
-    cy.location("pathname").should("equal", "/fundamentals")
-
+    cy.location("pathname").should("equal", "/fundamentals");
 
     cy.getDataTest("nav-forms").click();
-    cy.location("pathname").should("equal", "/forms")
+    cy.location("pathname").should("equal", "/forms");
 
     // Need to come back to an figure out failure - Timed out retrying after 4000ms: expected '/forms' to equal '/examples'
     // cy.getDataTest("nav-examples").click();
     // cy.location("pathname").should("equal", "/examples")
 
     cy.getDataTest("nav-component").click();
-    cy.location("pathname").should("equal", "/component")
+    cy.location("pathname").should("equal", "/component");
 
     cy.getDataTest("nav-best-practices").click();
-    cy.location("pathname").should("equal", "/best-practices")
-
+    cy.location("pathname").should("equal", "/best-practices");
   });
   it.only("intercepts", () => {
     cy.intercept("POST", "http://localhost:3000/examples", {
       // body: {
       //   message: "Successfully intercepted POST request"
       // }
-      fixture: "example.json"
-    })
+      fixture: "example.json",
+    });
     cy.getDataTest("post-button").click();
-  })
+  });
 
   it.only("grudges", () => {
-    cy.contains(/add some grudges/i)
+    cy.contains(/add some grudges/i);
 
-    cy.getDataTest("grudge-list").within(()=> {
-      cy.get("li").should("have.length", 0)
-    })
-    cy.getDataTest("grudge-input").within(()=> {
-      cy.get("input").type("Jason")
-    })
-    cy.getDataTest("add-grudge-button").click()
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 0);
+    });
+    cy.getDataTest("grudge-input").within(() => {
+      cy.get("input").type("Jason");
+    });
+    cy.getDataTest("add-grudge-button").click();
 
-    cy.getDataTest("grudge-list").within(()=> {
-      cy.get("li").should("have.length", 1)
-    })
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 1);
+    });
 
-    cy.getDataTest("grudge-input").within(()=> {
-      cy.get("input").type("Nick")
-    })
-    cy.getDataTest("add-grudge-button").click()
+    cy.getDataTest("grudge-input").within(() => {
+      cy.get("input").type("Nick");
+    });
+    cy.getDataTest("add-grudge-button").click();
 
-    cy.getDataTest("grudge-list").within(()=> {
-      cy.get("li").should("have.length", 2)
-      cy.get("li").its(0).should("contain.text", "Jason")
-    })
-    
-  })
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 2);
+      cy.get("li").its(0).should("contain.text", "Jason");
+    });
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li")
+        .its(0)
+        .within(() => {
+          cy.get("button").click();
+        });
+    });
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 1);
+      cy.get("li").its(0).should("contain.text", "Nick");
+    });
+  });
 });
-
