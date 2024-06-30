a686622895116ddb2f302c3bb44d8679e3ac38ce
Jason <jasonetienne1@gmail.com>
2024-06-30 13:29:33 +0100

    testing component has the specified list amount

--- cypress-course/cypress/component/Accordion.cy.jsx
+++ cypress-course/cypress/component/Accordion.cy.jsx
@@ -31,5 +31,8 @@ const items = [
 describe("Accordion.cy.jsx", () => {
   it("Items Accordion", () => {
     cy.mount(<ItemsAccordion items={items} />);
+    cy.getDataTest("accordion-wrapper").within(() => {
+      cy.get("[data-test^='accordion-item']").should("have.length", 3)
+    })
   });
 });
